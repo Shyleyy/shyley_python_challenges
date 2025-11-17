2be597e4203f00d80fdaddcf52ba4dce9b0aa31a
Shyley Charbeneau <shyley.charbeneau@gmail.com>
2025-11-17 17:26:51 -0500

    reversed string

---
+++ reversing.py
@@ -0,0 +1,9 @@
+def reverse_string(s):
+    """Return the reverse of the input string s."""
+    return s[::-1]
+    
+
+input_string = input("Enter a string to reverse: ")
+reversed_string = reverse_string(input_string)
+print(f"Original string: {input_string}")
+print(f"Reversed string: {reversed_string}")
